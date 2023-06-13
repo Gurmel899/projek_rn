abf02ffd371bff0a2ad0481da41f1e1e76ab32a9
Jhon Vebrianto Girsang <jvg.inq@gmail.com>
2023-06-13 15:24:21 +0700

    [MOD] null data in image logo

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,7 +7,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <link rel="icon" href="{{ url($setting->path_logo) }}" type="image/png">
+    {{-- <link rel="icon" href="{{ url($setting->path_logo) }}" type="image/png"> --}}
 
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="{{ asset('AdminLTE-2/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
@@ -30,7 +30,7 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition login-page">
-    
+
     @yield('login')
 
     <!-- jQuery 3 -->
